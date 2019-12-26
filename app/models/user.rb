c1829e99bbb60f8c4192352b8fad352ff84d8135
sudo.shunsuke <s_sudo_j@yahoo.co.jp>
2019-12-26 02:21:42 +0000

    kadai-micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: "Relationship", foreign_key: "follow_id"
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favoritings, through: :favorites, source: :micropost
+  has_many :reverses_of_favorites, class_name: "Favorite", foreign_key: "micropost_id"
+  has_many :favoriters, through: :reverses_of_favorites, source: :user
   
   def follow(oter_user)
     unless self == oter_user
@@ -29,5 +33,18 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def favorite(other_micropost)
+      self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+  end
+  
+  def unfavorite(other_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favoriting?(other_micropost)
+    self.favoritings.include?(other_micropost)
   end
 end
